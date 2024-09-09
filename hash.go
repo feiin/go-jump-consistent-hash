d55753b64183c967ece7d136595b1218988b491e
Yin Yang <feiin@qq.com>
2024-09-09 22:49:04 +0800

    fix: jump hash

--- hash.go
+++ hash.go
@@ -1,8 +1,6 @@
 // A Fast, Minimal Memory, Consistent Hash Algorithm https://arxiv.org/pdf/1406.2294
 package hash
 
-import "math"
-
 // JumpConsistentHash returns the bucket index for the given key using Jump Consistent Hash algorithm.
 // numBuckets must be > 0
 // the bucket index in the range [0, buckets)
@@ -12,7 +10,7 @@ func JumpConsistentHash(key uint64, numBuckets int32) int32 {
 	for j < int64(numBuckets) {
 		b = j
 		key = key*2862933555777941757 + 1
-		j = (b + 1) * int64(math.Pow(2, 31)/float64((key>>33)+1))
+		j = int64(float64(b+1) * float64(int64(1)<<31) / float64((key>>33)+1))
 	}
 	return int32(b)
 }
